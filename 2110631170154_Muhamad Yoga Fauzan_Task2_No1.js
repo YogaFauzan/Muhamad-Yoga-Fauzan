90888335e247098cfd2d5efd8c0f140a9db712ef
YogaFauzan <128561417+YogaFauzan@users.noreply.github.com>
2023-03-24 16:51:14 +0700

    Add files via upload

---
+++ 2110631170154_Muhamad Yoga Fauzan_Task2_No1.js
@@ -0,0 +1,15 @@
+/*
+NPM     : 2110631170154
+Nama    : Muh. Yoga Fauzan 
+Kelas   : 4C
+Tugas no 1
+*/
+
+let books = [
+    { title: "Filosofi Teras", author: "Henry Manampiring" },
+    { title: "The Power of Habit", author: "Charles Duhigg" },
+    { title: "The Art of Thinking Clearly", author: "Rolf Dobelli" }
+];
+for (let book of books) {
+    console.log(`Title: ${book.title} \nAuthor: ${book.author}\n`);
+}
